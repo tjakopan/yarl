ce5f2ce471bfdbf8bc9a935e920c9c6f238fd4ea
Tomislav Jakopanec <tjakopan@gmail.com>
2019-08-07 00:06:41 +0200

    Refactor exception predicate from interface to type alias

--- yarl-core/src/main/kotlin/hr/tjakopan/yarl/ExceptionPredicates.kt
+++ yarl-core/src/main/kotlin/hr/tjakopan/yarl/ExceptionPredicates.kt
@@ -1,7 +1,7 @@
 package hr.tjakopan.yarl
 
 /**
- * A collection of predicated used to define whether a policy hndles a given [Throwable].
+ * A collection of predicates used to define whether a policy handles a given [Throwable].
  */
 class ExceptionPredicates(private val predicates: MutableList<ExceptionPredicate>) {
   companion object {
@@ -18,8 +18,17 @@ class ExceptionPredicates(private val predicates: MutableList<ExceptionPredicate
     predicates += predicate
   }
 
+  /**
+   * Assess whether the passed [Throwable], [ex], matches any of the predicates.
+   *
+   * It the .handleCause() method was used when configuring the policy, predicates may test whether any causes of [ex]
+   * match and may return a matching cause.
+   *
+   * @param ex The exception to assess against the predicates.
+   * @return The first exception to match a predicate; or null, if no match is found.
+   */
   fun firstMatchOrNull(ex: Throwable): Throwable? {
-    return predicates.map { it.test(ex) }
+    return predicates.map { it(ex) }
       .firstOrNull { it != null }
   }
 }
