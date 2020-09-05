4e8ca3085fe56b3ceca4b0e57844dad6b21d6cb5
Tomislav Jakopanec <tjakopan@gmail.com>
2020-09-05 23:01:02 +0200

    Retry policy tests - wip

---
+++ yarl-core-java/src/test/java/hr/tjakopan/yarl/retry/AsyncRetryForeverTest.java
@@ -0,0 +1,409 @@
+package hr.tjakopan.yarl.retry;
+
+import hr.tjakopan.yarl.Context;
+import hr.tjakopan.yarl.test.helpers.AsyncPolicyUtils;
+import kotlin.Unit;
+import kotlin.jvm.functions.Function0;
+import org.junit.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static hr.tjakopan.yarl.Functions.fromConsumer;
+import static hr.tjakopan.yarl.Functions.fromConsumer3Async;
+import static org.assertj.core.api.Assertions.*;
+
+public class AsyncRetryForeverTest {
+  @Test
+  public void shouldNotThrowRegardlessOfHowManyTimesTheSpecifiedExceptionIsRaised() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+
+    AsyncPolicyUtils.raiseExceptions(policy, 3, i -> new ArithmeticException())
+      .join();
+  }
+
+  @Test
+  public void shouldNotThrowRegardlessOfHowManyTimesOneOfTheSpecifiedExceptionIsRaised() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .handle(IllegalArgumentException.class)
+      .retryForever();
+
+    AsyncPolicyUtils.raiseExceptions(policy, 3, i -> new IllegalArgumentException())
+      .join();
+  }
+
+  @Test
+  public void shouldThrowWhenExceptionThrownIsNotTheSpecifiedExceptionType() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+
+    assertThatExceptionOfType(CompletionException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new NullPointerException())
+        .join())
+      .withCauseInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  public void shouldThrowWhenExceptionThrownIsNotOneOfTheSpecifiedExceptionTypes() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .handle(IllegalArgumentException.class)
+      .retryForever();
+
+    assertThatExceptionOfType(CompletionException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new NullPointerException())
+        .join())
+      .withCauseInstanceOf(NullPointerException.class);
+  }
+
+  @Test
+  public void shouldThrowWhenSpecifiedExceptionPredicateIsNotSatisfied() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class, e -> false)
+      .retryForever();
+
+    assertThatExceptionOfType(CompletionException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new ArithmeticException())
+        .join())
+      .withCauseInstanceOf(ArithmeticException.class);
+  }
+
+  @Test
+  public void shouldThrowWhenNoneOfTheSpecifiedExceptionPredicatesAreSatisfied() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class, e -> false)
+      .handle(IllegalArgumentException.class, e -> false)
+      .retryForever();
+
+    assertThatExceptionOfType(CompletionException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new IllegalArgumentException())
+        .join())
+      .withCauseInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  public void shouldNotThrowWhenSpecifiedExceptionPredicateIsSatisfied() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class, e -> true)
+      .retryForever();
+
+    AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new ArithmeticException())
+      .join();
+  }
+
+  @Test
+  public void shouldNotThrowWhenOneOfTheSpecifiedExceptionPredicatesAreSatisfied() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class, e -> true)
+      .handle(IllegalArgumentException.class, e -> true)
+      .retryForever();
+
+    AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new IllegalArgumentException())
+      .join();
+  }
+
+  @Test
+  public void shouldCallOnRetryOnEachRetryWithTheCurrentException() {
+    final var expectedExceptions = List.of("Exception #1", "Exception #2", "Exception #3");
+    final var retryExceptions = new ArrayList<Throwable>(3);
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(outcome -> (i, c) -> outcome.onFailure(fromConsumer(retryExceptions::add))));
+
+    AsyncPolicyUtils.raiseExceptions(policy, 3, i -> new ArithmeticException("Exception #" + i))
+      .join();
+
+    assertThat(retryExceptions.stream()
+      .map(Throwable::getMessage))
+      .containsExactlyElementsOf(expectedExceptions);
+  }
+
+  @Test
+  public void shouldCallOnRetryOnEachRetryWithThePassedContext() {
+    final var context = new AtomicReference<Context>();
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(d -> (i, ctx) -> context.set(ctx)));
+
+    AsyncPolicyUtils.raiseExceptions(
+      policy,
+      Context.builder()
+        .contextData(new HashMap<>() {{
+          put("key1", "value1");
+          put("key2", "value2");
+        }})
+        .build(),
+      1,
+      i -> new ArithmeticException())
+      .join();
+
+    assertThat(context.get()).isNotNull();
+    assertThat(context.get().getContextData()).containsKeys("key1", "key2")
+      .containsValues("value1", "value2");
+  }
+
+  @Test
+  public void shouldCallOnRetryOnEachRetryWithTheCurrentRetryCount() {
+    final var expectedRetryCounts = List.of(1, 2, 3);
+    final var retryCounts = new ArrayList<Integer>(3);
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(d -> (retryCount, c) -> retryCounts.add(retryCount)));
+
+    AsyncPolicyUtils.raiseExceptions(policy, 3, i -> new ArithmeticException())
+      .join();
+
+    assertThat(retryCounts).containsExactlyElementsOf(expectedRetryCounts);
+  }
+
+  @Test
+  public void contextShouldBeEmptyIfExecuteNotCalledWithContext() {
+    final var capturedContext = new AtomicReference<Context>();
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(r -> (i, ctx) -> capturedContext.set(ctx)));
+
+    AsyncPolicyUtils.raiseExceptions(policy, 1, i -> new ArithmeticException())
+      .join();
+
+    assertThat(capturedContext.get()).isNotNull();
+    assertThat(capturedContext.get().getPolicyWrapKey()).isNull();
+    assertThat(capturedContext.get().getPolicyKey()).isNotNull();
+    assertThat(capturedContext.get().getOperationKey()).isNull();
+    assertThat(capturedContext.get().getContextData()).isEmpty();
+  }
+
+  @Test
+  public void shouldCreateNewContextForEachCallToExecute() {
+    final var contextValue = new AtomicReference<String>();
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(r -> (i, ctx) -> contextValue.set(ctx.getContextData().get("key").toString())));
+
+    AsyncPolicyUtils.raiseExceptions(policy,
+      Context.builder()
+        .contextData(new HashMap<>() {{
+          put("key", "original_value");
+        }}).build(),
+      1,
+      i -> new ArithmeticException())
+      .join();
+
+    assertThat(contextValue.get()).isEqualTo("original_value");
+
+    AsyncPolicyUtils.raiseExceptions(policy,
+      Context.builder()
+        .contextData(new HashMap<>() {{
+          put("key", "new_value");
+        }}).build(),
+      1,
+      i -> new ArithmeticException())
+      .join();
+
+    assertThat(contextValue.get()).isEqualTo("new_value");
+  }
+
+  @Test
+  public void shouldNotCallOnRetryWhenNoRetriesArePerformed() {
+    final var retryCalled = new AtomicBoolean(false);
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(r -> (i, c) -> retryCalled.set(true)));
+
+    assertThatThrownBy(() -> AsyncPolicyUtils.raiseExceptions(policy, 1,
+      i -> new IllegalArgumentException())
+      .join())
+      .isInstanceOf(CompletionException.class)
+      .hasCauseInstanceOf(IllegalArgumentException.class);
+    assertThat(retryCalled.get()).isFalse();
+  }
+
+  @Test
+  public void shouldWaitAsynchronouslyForAsyncOnRetryDelegate() {
+    final var duration = Duration.ofMillis(200);
+    final var executeDelegateInvocations = new AtomicInteger(0);
+    final var executeDelegateInvocationsWhenOnRetryExits = new AtomicInteger(0);
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retry((r, i, c) -> {
+        final var executor = CompletableFuture.delayedExecutor(duration.toMillis(), TimeUnit.MILLISECONDS);
+        return CompletableFuture
+          .runAsync(() -> executeDelegateInvocationsWhenOnRetryExits.set(executeDelegateInvocations.get()), executor)
+          .thenApplyAsync(v -> Unit.INSTANCE);
+      });
+
+    policy.executeAsync(() -> {
+      executeDelegateInvocations.incrementAndGet();
+      if (executeDelegateInvocations.get() == 1) {
+        throw new ArithmeticException();
+      }
+      return CompletableFuture.completedFuture(null);
+    })
+      .join();
+
+    assertThat(executeDelegateInvocationsWhenOnRetryExits.get()).isEqualTo(1);
+    assertThat(executeDelegateInvocations.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void shouldExecuteActionWhenNonFaultingAndNotCancelled() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    AsyncPolicyUtils.raiseExceptions(policy, 0, action, i -> new ArithmeticException())
+      .join();
+
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldNotExecuteActionWhenCancelledBeforeExecute() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Executor executor = CompletableFuture.delayedExecutor(200, TimeUnit.MILLISECONDS);
+    final CompletableFuture<Void> dummyFuture = CompletableFuture.runAsync(() -> {
+    }, executor);
+    final Function0<CompletableFuture<Void>> action = () ->
+      dummyFuture.thenComposeAsync(v -> {
+        attemptsInvoked.incrementAndGet();
+        return CompletableFuture.completedFuture(null);
+      });
+
+    dummyFuture.cancel(true);
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1 + 3, action, i -> new ArithmeticException())
+        .join());
+    assertThat(attemptsInvoked.get()).isEqualTo(0);
+  }
+
+  @Test
+  public void shouldReportCancellationDuringOtherwiseNonFaultingActionExecutionAndCancelFurtherRetries() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptionsAndOrCancellation(policy, 0, 1, action,
+        i -> new ArithmeticException())
+        .join());
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldReportCancellationDuringFaultingInitialActionExecutionAndCancelFurtherRetries() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptionsAndOrCancellation(policy, 1 + 3, 1,
+        action, i -> new ArithmeticException())
+        .join());
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldReportCancellationDuringFaultingRetriedActionExecutionAndCancelFurtherRetries() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptionsAndOrCancellation(policy, 1 + 3, 2,
+        action, i -> new ArithmeticException())
+        .join());
+    assertThat(attemptsInvoked.get()).isEqualTo(2);
+  }
+
+  @Test
+  public void shouldReportCancellationAfterFaultingActionExecutionAndCancelFurtherRetriesIfOnRetryInvokesCancellation() {
+    final var policy = AsyncRetryPolicy.<Void>builder()
+      .handle(ArithmeticException.class)
+      .retryForeverAsync(fromConsumer3Async(r -> (i, c) -> {
+        throw new CancellationException();
+      }));
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      AsyncPolicyUtils.raiseExceptions(policy, 1 + 3, action, i -> new ArithmeticException())
+        .join());
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldExecuteFunctionReturningValueWhenNotCancelled() {
+    final var policy = AsyncRetryPolicy.<Boolean>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+
+    final var result = AsyncPolicyUtils.raiseExceptions(policy, 0, action, true,
+      i -> new ArithmeticException())
+      .join();
+
+    assertThat(result).isTrue();
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+
+  @Test
+  public void shouldHonourAndReportCancellationDuringFunctionExecution() {
+    final var policy = AsyncRetryPolicy.<Boolean>builder()
+      .handle(ArithmeticException.class)
+      .retryForever();
+    final var attemptsInvoked = new AtomicInteger(0);
+    final Function0<CompletableFuture<Void>> action = () -> {
+      attemptsInvoked.incrementAndGet();
+      return CompletableFuture.completedFuture(null);
+    };
+    final AtomicReference<Boolean> result = new AtomicReference<>();
+
+    assertThatExceptionOfType(CancellationException.class).isThrownBy(() ->
+      result.set(AsyncPolicyUtils.raiseExceptionsAndOrCancellation(policy, 0, 1,
+        action, true, i -> new ArithmeticException())
+        .join()));
+    assertThat(result.get()).isNull();
+    assertThat(attemptsInvoked.get()).isEqualTo(1);
+  }
+}
---
+++ yarl-core/src/test/kotlin/hr/tjakopan/yarl/retry/AsyncRetryForeverTest.kt
@@ -0,0 +1,349 @@
+package hr.tjakopan.yarl.retry
+
+import hr.tjakopan.yarl.Context
+import hr.tjakopan.yarl.Policy
+import hr.tjakopan.yarl.test.helpers.raiseExceptions
+import hr.tjakopan.yarl.test.helpers.raiseExceptionsAndOrCancellation
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.test.runBlockingTest
+import org.assertj.core.api.Assertions.assertThat
+import java.time.Duration
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+@ExperimentalCoroutinesApi
+class AsyncRetryForeverTest {
+  @Test
+  fun shouldNotThrowRegardlessOfHowManyTimesTheSpecifiedExceptionIsRaised() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+
+    policy.raiseExceptions(3) { ArithmeticException() }
+  }
+
+  @Test
+  fun shouldNotThrowRegardlessOfHowManyTimesOneOfTheSpecifiedExceptionIsRaised() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .handle(IllegalArgumentException::class)
+      .retryForever()
+
+    policy.raiseExceptions(3) { IllegalArgumentException() }
+  }
+
+  @Test
+  fun shouldThrowWhenExceptionThrownIsNotTheSpecifiedExceptionType() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+
+    assertFailsWith(NullPointerException::class) {
+      policy.raiseExceptions(1) { NullPointerException() }
+    }
+  }
+
+  @Test
+  fun shouldThrowWhenExceptionThrownIsNotOneOfTheSpecifiedExceptionTypes() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .handle(IllegalArgumentException::class)
+      .retryForever()
+
+    assertFailsWith(NullPointerException::class) {
+      policy.raiseExceptions(1) { NullPointerException() }
+    }
+  }
+
+  @Test
+  fun shouldThrowWhenSpecifiedExceptionPredicateIsNotSatisfied() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class) { false }
+      .retryForever()
+
+    assertFailsWith(ArithmeticException::class) {
+      policy.raiseExceptions(1) { ArithmeticException() }
+    }
+  }
+
+  @Test
+  fun shouldThrowWhenNoneOfTheSpecifiedExceptionPredicatesAreSatisfied() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class) { false }
+      .handle(IllegalArgumentException::class) { false }
+      .retryForever()
+
+    assertFailsWith(IllegalArgumentException::class) {
+      policy.raiseExceptions(1) { IllegalArgumentException() }
+    }
+  }
+
+  @Test
+  fun shouldNotThrowWhenSpecifiedExceptionPredicateIsSatisfied() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class) { true }
+      .retryForever()
+
+    policy.raiseExceptions(1) { ArithmeticException() }
+  }
+
+  @Test
+  fun shouldNotThrowWhenOneOfTheSpecifiedExceptionPredicatesAreSatisfied() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class) { true }
+      .handle(IllegalArgumentException::class) { true }
+      .retryForever()
+
+    policy.raiseExceptions(1) { IllegalArgumentException() }
+  }
+
+  @Test
+  fun shouldCallOnRetryOnEachRetryWithTheCurrentException() = runBlockingTest {
+    val expectedExceptions = listOf("Exception #1", "Exception #2", "Exception #3")
+    val retryExceptions = mutableListOf<Throwable>()
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { outcome, _, _ -> outcome.onFailure { retryExceptions.add(it) } }
+
+    policy.raiseExceptions(3) { ArithmeticException("Exception #$it") }
+
+    assertThat(retryExceptions.map { it.message }).containsExactlyElementsOf(expectedExceptions)
+  }
+
+  @Test
+  fun shouldCallOnRetryOnEachRetryWithThePassedContext() = runBlockingTest {
+    var context: Context? = null
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, _, ctx -> context = ctx }
+
+    policy.raiseExceptions(
+      Context(contextData = mapOf("key1" to "value1", "key2" to "value2")),
+      1
+    ) { ArithmeticException() }
+
+    assertThat(context).isNotNull
+    assertThat(context?.contextData).containsKeys("key1", "key2")
+      .containsValues("value1", "value2")
+  }
+
+  @Test
+  fun shouldCallOnRetryOnEachRetryWithTheCurrentRetryCount() = runBlockingTest {
+    val expectedRetryCounts = listOf(1, 2, 3)
+    val retryCounts = mutableListOf<Int>()
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, retryCount, _ -> retryCounts.add(retryCount) }
+
+    policy.raiseExceptions(3) { ArithmeticException() }
+
+    assertThat(retryCounts).containsExactlyElementsOf(expectedRetryCounts)
+  }
+
+  @Test
+  fun contextShouldBeEmptyIfExecuteNotCalledWithContext() = runBlockingTest {
+    var capturedContext: Context? = null
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, _, ctx -> capturedContext = ctx }
+
+    policy.raiseExceptions(1) { ArithmeticException() }
+
+    assertThat(capturedContext).isNotNull
+    assertThat(capturedContext?.policyWrapKey).isNull()
+    assertThat(capturedContext?.policyKey).isNotNull()
+    assertThat(capturedContext?.operationKey).isNull()
+    assertThat(capturedContext?.contextData).isEmpty()
+  }
+
+  @Test
+  fun shouldCreateNewContextForEachCallToExecute() = runBlockingTest {
+    var contextValue: String? = null
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, _, ctx -> contextValue = ctx.contextData["key"].toString() }
+
+    policy.raiseExceptions(Context(contextData = mapOf("key" to "original_value")), 1) { ArithmeticException() }
+
+    assertThat(contextValue).isEqualTo("original_value")
+
+    policy.raiseExceptions(Context(contextData = mapOf("key" to "new_value")), 1) { ArithmeticException() }
+
+    assertThat(contextValue).isEqualTo("new_value")
+  }
+
+  @Test
+  fun shouldNotCallOnRetryWhenNoRetriesArePerformed() = runBlockingTest {
+    var retryCalled = false
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, _, _ -> retryCalled = true }
+
+    assertFailsWith(IllegalArgumentException::class) {
+      policy.raiseExceptions(1) { IllegalArgumentException() }
+    }
+    assertThat(retryCalled).isFalse()
+  }
+
+  @Test
+  fun shouldWaitAsynchronouslyForAsyncOnRetryDelegate() = runBlockingTest {
+    val duration = Duration.ofMillis(200)
+    var executeDelegateInvocations = 0
+    var executeDelegateInvocationsWhenOnRetryExits = 0
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever { _, _, _ ->
+        delay(duration.toMillis())
+        executeDelegateInvocationsWhenOnRetryExits = executeDelegateInvocations
+      }
+
+    policy.execute {
+      executeDelegateInvocations++
+      if (executeDelegateInvocations == 1) {
+        throw ArithmeticException()
+      }
+    }
+
+    assertThat(executeDelegateInvocationsWhenOnRetryExits).isEqualTo(1)
+    assertThat(executeDelegateInvocations).isEqualTo(2)
+  }
+
+  @Test
+  fun shouldExecuteActionWhenNonFaultingAndNotCancelled() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+    var attemptsInvoked = 0
+    val onExecute: () -> Unit = { attemptsInvoked++ }
+
+    policy.raiseExceptions(0, onExecute) { ArithmeticException() }
+
+    assertThat(attemptsInvoked).isEqualTo(1)
+  }
+
+  @Test
+  fun shouldNotExecuteActionWhenCancelledBeforeExecute() {
+    assertFailsWith(CancellationException::class) {
+      runBlockingTest {
+        val policy = Policy.asyncRetry<Unit>()
+          .handle(ArithmeticException::class)
+          .retryForever()
+        var attemptsInvoked = 0
+        val onExecute: () -> Unit = { attemptsInvoked++ }
+
+        coroutineContext.cancel()
+
+        assertFailsWith(CancellationException::class) {
+          policy.raiseExceptions(1 + 3, onExecute) { ArithmeticException() }
+        }
+        assertThat(attemptsInvoked).isEqualTo(0)
+      }
+    }
+  }
+
+  @Test
+  fun shouldReportCancellationDuringOtherwiseNonFaultingActionExecutionAndCancelFurtherRetries() =
+    runBlockingTest {
+      val policy = Policy.asyncRetry<Unit>()
+        .handle(ArithmeticException::class)
+        .retryForever()
+      var attemptsInvoked = 0
+      val onExecute: () -> Unit = { attemptsInvoked++ }
+
+      assertFailsWith(CancellationException::class) {
+        policy.raiseExceptionsAndOrCancellation(0, 1, onExecute) {
+          ArithmeticException()
+        }
+      }
+      assertThat(attemptsInvoked).isEqualTo(1)
+    }
+
+  @Test
+  fun shouldReportCancellationDuringFaultingInitialActionExecutionAndCancelFurtherRetries() =
+    runBlockingTest {
+      val policy = Policy.asyncRetry<Unit>()
+        .handle(ArithmeticException::class)
+        .retryForever()
+      var attemptsInvoked = 0
+      val onExecute: () -> Unit = { attemptsInvoked++ }
+
+      assertFailsWith(CancellationException::class) {
+        policy.raiseExceptionsAndOrCancellation(1 + 3, 1, onExecute) {
+          ArithmeticException()
+        }
+      }
+      assertThat(attemptsInvoked).isEqualTo(1)
+    }
+
+  @Test
+  fun shouldReportCancellationDuringFaultingRetriedActionExecutionAndCancelFurtherRetries() = runBlockingTest {
+    val policy = Policy.asyncRetry<Unit>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+    var attemptsInvoked = 0
+    val onExecute: () -> Unit = { attemptsInvoked++ }
+
+    assertFailsWith(CancellationException::class) {
+      policy.raiseExceptionsAndOrCancellation(1 + 3, 2, onExecute) {
+        ArithmeticException()
+      }
+    }
+    assertThat(attemptsInvoked).isEqualTo(2)
+  }
+
+  @Test
+  fun shouldReportCancellationAfterFaultingActionExecutionAndCancelFurtherRetriesIfOnRetryInvokesCancellation() {
+    assertFailsWith(CancellationException::class) {
+      runBlockingTest {
+        val policy = Policy.asyncRetry<Unit>()
+          .handle(ArithmeticException::class)
+          .retryForever() { _, _, _ -> coroutineContext.cancel() }
+        var attemptsInvoked = 0
+        val onExecute: () -> Unit = { attemptsInvoked++ }
+
+        assertFailsWith(CancellationException::class) {
+          policy.raiseExceptions(1 + 3, onExecute) { ArithmeticException() }
+        }
+        assertThat(attemptsInvoked).isEqualTo(1)
+      }
+    }
+  }
+
+  @Test
+  fun shouldExecuteFunctionReturningValueWhenNotCancelled() = runBlockingTest {
+    val policy = Policy.asyncRetry<Boolean>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+    var attemptsInvoked = 0
+    val onExecute: () -> Unit = { attemptsInvoked++ }
+
+    val result = policy.raiseExceptions(0, onExecute, true) {
+      ArithmeticException()
+    }
+
+    assertThat(result).isTrue()
+    assertThat(attemptsInvoked).isEqualTo(1)
+  }
+
+  @Test
+  fun shouldHonourAndReportCancellationDuringFunctionExecution() = runBlockingTest {
+    val policy = Policy.asyncRetry<Boolean>()
+      .handle(ArithmeticException::class)
+      .retryForever()
+    var attemptsInvoked = 0
+    val onExecute: () -> Unit = { attemptsInvoked++ }
+    var result: Boolean? = null
+
+    assertFailsWith(CancellationException::class) {
+      result = policy.raiseExceptionsAndOrCancellation(
+        0, 1, onExecute,
+        true
+      ) { ArithmeticException() }
+    }
+    assertThat(result).isNull()
+    assertThat(attemptsInvoked).isEqualTo(1)
+  }
+}
